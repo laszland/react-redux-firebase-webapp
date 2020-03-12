5045291f98d5ef160ae3201ec43e8ad5eda48e55
Laci <laszlo.roland.kiss@gmail.com>
2020-03-12 21:41:21 +0100

    create projectDeatails and start to code sighIn

--- projectplan/src/components/projects/ProjectList.js
+++ projectplan/src/components/projects/ProjectList.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Laszlo Roland Kiss</p>
-          <p className="grey-text">12th of March, 8:21 pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Laszlo Roland Kiss</p>
-          <p className="grey-text">12th of March, 8:21 pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Laszlo Roland Kiss</p>
-          <p className="grey-text">12th of March, 8:21 pm</p>
-        </div>
-      </div>
+      <ProjectSummary/>
+      <ProjectSummary/>
+      <ProjectSummary/>
+      <ProjectSummary/>
     </div>
   )
 }
