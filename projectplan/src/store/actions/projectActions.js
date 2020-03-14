fedbe1fc9c69c9f1aa54ac7d7647a16a164c4170
Laci <laszlo.roland.kiss@gmail.com>
2020-03-14 21:15:15 +0100

    add Firebaseand Firestore to project

--- projectplan/src/store/actions/projectActions.js
+++ projectplan/src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make some async DB call
     dispatch({ type: 'CREATE_PROJECT', project})
   }
